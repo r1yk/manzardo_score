7a6fae7bd82e6a5ccbb9b3068e0dbb1bb8f59d26
Roy Kresge <49421368+r1yk@users.noreply.github.com>
2025-02-09 16:24:43 -0500

    bugs

--- calculate.py
+++ calculate.py
@@ -5,13 +5,17 @@ from circle import smallest_circle
 
 
 def window_function(all_homers: List[dict]):
-    for n in range(2, 5):
-        # n = 2
+    for n in range(2, 20):
         print("N =", n)
+        homer_groups_of_n = []
         for start_index in range(0, len(all_homers) - n):
             homers = all_homers[start_index : start_index + n]
             smallest = smallest_circle(homers)
-            print("SMALLEST", smallest)
+            # print(smallest)
+            homer_groups_of_n.append(smallest)
+
+        homer_groups_of_n.sort(key=lambda circle: circle.radius)
+        print("SMALLEST:", homer_groups_of_n[0])
 
 
 with open("data/player_data.json") as json_file:
--- circle.py
+++ circle.py
@@ -15,16 +15,17 @@ class Coordinate:
     y: float
 
     def is_within(self, c: Circle) -> bool:
-        distance_from_center = math.sqrt(
-            (c.center.x - self.x) ** 2 + (c.center.y - self.y) ** 2
-        )
-        return distance_from_center <= c.radius
+        return distance_between(self, c.center) <= c.radius
 
 
 @dataclass
 class Circle:
     radius: float
     center: Coordinate
+
+
+def distance_between(a: Coordinate, b: Coordinate):
+    return math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2)
 
 
 def smallest_circle(homers: List[dict]) -> Circle:
@@ -36,27 +37,47 @@ def smallest_circle(homers: List[dict]) -> Circle:
 
 
 def welzl(all_points: Set[Coordinate], boundary_points: Set[Coordinate]) -> Circle:
-    # print(all_points, boundary_points)
-
     if len(all_points) == 0 or len(boundary_points) == 3:
-        return trivial(all_points, boundary_points)
+        return trivial(boundary_points)
 
-    first_point = all_points.pop()
-    circle = welzl(all_points, boundary_points)
-    if first_point.is_within(circle):
+    random_point = random_point_in_set(all_points)
+    circle = welzl(all_points.difference({random_point}), boundary_points)
+
+    if random_point.is_within(circle):
         return circle
 
-    return welzl(all_points, boundary_points.union({first_point}))
+    return welzl(
+        all_points.difference({random_point}), boundary_points.union({random_point})
+    )
 
 
-def trivial(all_points: Set[Coordinate], boundary_points: Set[Coordinate]):
-    if len(all_points) == 0 or len(boundary_points) == 1:
+def random_point_in_set(points: Set[Coordinate]) -> Coordinate:
+    # TODO
+    return list(points)[0]
+
+
+def trivial(boundary_points: Set[Coordinate]):
+    if len(boundary_points) == 0:
         return Circle(radius=0, center=Coordinate(0, 0))
-    elif len(boundary_points) == 2:
-        return Circle(radius=50, center=Coordinate(0, 0))
 
-    points = list(boundary_points)
-    return circumcircle(A=points[0], B=points[1], C=points[2])
+    elif len(boundary_points) == 1:
+        points = list(boundary_points)
+        return Circle(radius=0, center=points[0])
+
+    elif len(boundary_points) == 2:
+        points = list(boundary_points)
+        return Circle(
+            radius=distance_between(points[0], points[1]) / 2,
+            center=midpoint(points[0], points[1]),
+        )
+
+    else:
+        points = list(boundary_points)
+        return circumcircle(A=points[0], B=points[1], C=points[2])
+
+
+def midpoint(a: Coordinate, b: Coordinate) -> Coordinate:
+    return Coordinate(x=(a.x + b.x) / 2, y=(a.y + b.y) / 2)
 
 
 def circumcircle(A: Coordinate, B: Coordinate, C: Coordinate) -> Circle:
@@ -84,5 +105,4 @@ def circumcircle(A: Coordinate, B: Coordinate, C: Coordinate) -> Circle:
 
     # Compute circumradius
     radius = math.sqrt((A.x - Ux) ** 2 + (A.y - Uy) ** 2)
-
     return Circle(center=circumcenter, radius=radius)
